6ba33cc85cb234caaa978bb5bccc2b7c5a4f09f2
Alexey Ponomarev <mielofon@sprut.ru>
2018-10-15 13:17:19 +0300

    SumAllTails
    Change-Id: I7363df5a620db050ae26b4192aa24b805418d799

--- array/sum.go
+++ array/sum.go
@@ -14,3 +14,16 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) > 1 {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+	return sums
+}
