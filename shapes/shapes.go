b6dbb7886c187307639876b088a410f527511270
Alexey <mielofon@gmail.com>
2018-10-21 18:04:33 +0300

    shpe triangles

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,15 +1,58 @@
 package shapes
 
+import (
+	"math"
+)
+
+// Shape is innterface of shape with Area() and Perimeter() methods
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+// Rectangle is type passed rectangle with width and height
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
 // Perimeter return perimeter of square with width and height
-func Perimeter(r Rectangle) float64 {
-	return (r.width + r.height) * 2
+func (rectangle Rectangle) Perimeter() float64 {
+	return (rectangle.width + rectangle.height) * 2
 }
 
-func Area(r Rectangle) float64 {
-	return (r.width * r.height)
+// Area return area of square with width and height
+func (rectangle Rectangle) Area() float64 {
+	return (rectangle.width * rectangle.height)
+}
+
+// Circle is type passed circle with radius
+type Circle struct {
+	radius float64
+}
+
+// Perimeter return perimeter of circle with redius
+func (circle Circle) Perimeter() float64 {
+	return 2 * math.Pi * circle.radius
+}
+
+// Area return area of circle with redius
+func (circle Circle) Area() float64 {
+	return math.Pi * circle.radius * circle.radius
+}
+
+// Triangle is type shape triangle with base and height
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+// Perimeter return perimeter of triangle with base and height
+func (triangle Triangle) Perimeter() float64 {
+	return triangle.base + math.Sqrt(triangle.base*triangle.base/4+triangle.height*triangle.height)
+}
+
+// Area return area of circle with triangle with base and height
+func (triangle Triangle) Area() float64 {
+	return triangle.base * triangle.height / 2
 }
--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -13,11 +13,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{width: 12, height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{base: 12, height: 6}, hasArea: 36.0},
 	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
+			}
+		})
+
+	}
+
 }
